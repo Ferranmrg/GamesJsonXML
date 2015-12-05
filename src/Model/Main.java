c800d23b6c690926cd2333e6472f75da28f53764
Pedro Velasco <pedro@pedro.com>
2015-12-05 21:00:42 +0100

    Version 1.0 -Añadido interfaz grafica y JavaDoc

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,8 +1,78 @@
 package Model;
 
-public class Main {
+import java.io.IOException;
+
+import Model.View.RootLayoutController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	private RootLayoutController rootController;
+	
+	
+	/*
+	 *se ejecuta al empezar y inicia el metodo initScene()
+	 *@see initScene()
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		primaryStage.setTitle("GamesJson");
+		
+		
+		initScene();
+				
+	}
+	
+	/**
+	 * Inicia la escena y crea el stage con el rootLayout cargado
+	 * @see RootLayoutController
+	 */
+	public void initScene(){
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("View/rootLayout.fxml"));
+        loader.setController(rootController);
+        try {
+			rootLayout = (AnchorPane) loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        
+       
+        Scene scene = new Scene(rootLayout);
+     
+        
+        primaryStage.setScene(scene);
+        rootController = loader.getController();
+        
+        //pasar metodos de controler de esta clase
+        // no funciona because reasons
+        //rootController.setMain(null);
+
+        primaryStage.show();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Parser p = new Parser();
 		p.getListName("Starcraft");
+		launch(args);
+	
 	}
 }
+
---
+++ src/Model/View/RootLayoutController.java
@@ -0,0 +1,20 @@
+package Model.View;
+
+
+
+import javafx.fxml.FXML;
+
+public class RootLayoutController {
+	
+	
+	/**
+	 * Se ejecuta despues de cagar el fxml
+	 * 
+	 */
+	@FXML
+	private void initialize() {
+	
+	}
+
+
+}
