305668e85b9720bc8e30bfc1c713d8e0f4c131a6
Pedro Velasco <pedro@pedro.com>
2015-12-06 01:57:13 +0100

    Version 1.1 - Añadido el metodo getVbox

--- src/Model/View/RootLayoutController.java
+++ src/Model/View/RootLayoutController.java
@@ -3,18 +3,26 @@ package Model.View;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class RootLayoutController {
-	
+	private VBox vbox;
 	
 	/**
-	 * Se ejecuta despues de cagar el fxml
+	 * Se ejecuta despues de cargar el fxml
 	 * 
 	 */
 	@FXML
 	private void initialize() {
 	
 	}
-
+	/**
+	 * Devuelve el VBox de javafx en el se guardaran los label con
+	 * los nombres de los videojuegos referentes al que has buscado
+	 * @return vbox donde almacenar las label de los nombres de los juegos
+	 */
+	private VBox getVbox(){
+		return vbox;
+	}
 
 }
